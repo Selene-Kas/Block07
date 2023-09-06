11182d538e88f3130ba2eb49d7714e1c4db1bace
Kassandra Medina <kassandramedina@Kassandras-MBP.lan>
2023-09-05 20:03:20 -0700

    Modified my pesudocode and simplified my code

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
-console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+/* Pseudocode: 
+1. (Declare each variable with const)
+2. String Datatype with first phrase assigned to a variable.
+3. String DataType with second phrase assigned to a variable.
+4. Concatenate both strings by adding them with enclosed back-ticks(`)
 
-//alert("Hello World");
+5. Create a subtraction calculation to equal 10 to an assigned variable.
+6. Create a multiplication calculation to equal 40 to an assigned variable.
+7. create a division calculation to equal 39 to an assigned varible.
+8. Concatenate all three values with a dash between each number enclosed by back-ticks.
 
-/* 
-Pseudocode: 
-Made multiple strings with assigned values and concatenated them together to give the user 
-a message with the combination to the vault. 
-Arithmetic calculation using subtraction operator to equal the first combination of value 10. 
-Arithmetic calculation using multiplication operator to equal the second combination of value 40. 
-Arithmetic calculation using division operator to equal the third combination of value 39. 
-Concatenate all three values with a string with a dash between each munber.
-Display the vault codes with dialog box and script tag in HTML page or an alert popup dialog. 
+9. Create a console.log to add both concatenated strings and numbers values
+10. Create a dialog box with both concatenated strings and numbers values
 */
 
 
-/*Made two const strings named str1 and str2 with assigned values and concatenated them together 
-with variable strComplete and declared with const, and sum of str1 and str2.To give the user 
-a message with the combination to the vault. */
+// To give the user a message with the combination to the vault in an alert dialog box and on the console. 
 const str1 = "You have received this message because you have been chosen to open an important vault.";
 const str2 = "Here is the secret combination:";
-//const strComplete = `${str1} ${str2}`;
+const strComplete = `${str1} ${str2}`;
+  /* Expected output of strComplete: "You have received this message because you have been chosen to open an important vault. 
+  Here is the secret combination:" */
 
-// Created variable num1 with an arithmetic calculation for value 10
-const num1 = 15 - 5;
+
+const num1 = 15 - 5; //equals 10
 const num2 = 1 * 2 * 20;
 const num3 = 117 / 3;
 const numComplete =`${num1} - ${num2} - ${num3}`;
-console.log(numComplete);
+  // Expected output of numComplete: "10 - 40 - 39"
 
-
-
-//console.log(strComplete);
-// Send a dialog box with alert with the concatenation of both strings(variables str1 and str2) and all three numbers (variables num1, num2, num3).
-alert(str1 + str2 + " " + num1 + " - " + num2 + " - " + num3);
-//Expected output: "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
-
+console.log(strComplete + " " + numComplete);    
+alert(strComplete + " " + numComplete);          
+  /*Expected output: "You have received this message because you have been chosen to open an important vault.
+   Here is the secret combination: 10 - 40 -39" */
